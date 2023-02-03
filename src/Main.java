32151a7ca136cdccebdd30fcc3d2dcf7ac290502
kjur4293 <kjur4293@naver.com>
2023-02-03 21:44:42 +0900

    세팅

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("==게시판 v 0.1 ==");
+    System.out.println("== 프로그램 시작 ==");
+    System.out.println("명령");
+    String cmd = sc.nextLine();
+    System.out.println("입력된 명령어 : %s\n". cmd);
+    System.out.println("==프로그램 종료");
+
+    sc.close();
+  }
+}
